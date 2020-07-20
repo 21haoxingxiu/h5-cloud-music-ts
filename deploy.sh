d6817982014bb81a4809b03c03cdc48d6e41a7a7
占佳敏 <zhanjiamin@xiongdi.cn>
2020-07-20 16:30:50 +0800

    上传build

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+###
+ # @Description: new file
+ # @Autor: zhan
+ # @Date: 2020-07-20 14:07:55
+ # @LastEditors: zhan
+ # @LastEditTime: 2020-07-20 14:29:51
+### 
+# 当发生错误时中止脚本
+set -e
+
+# 构建
+npm run build
+
+# cd 到构建输出的目录下 
+cd build
+
+# 部署到自定义域名，没有自定义域名可注释掉
+echo 'clearlove666.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 部署到 https://<USERNAME>.github.io
+git push -f https://github.com/21haoxingxiu/21haoxingxiu.github.io.git master
+
+cd -
